d5ac0770d06b358f0b750a8ab2f2bffd1b5c9be2
FloYoutube54 <48404856+FloYoutube54@users.noreply.github.com>
2019-10-17 18:24:10 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
-bot.login("NjM0NDIzOTQzNDkwNDM3MTQw.XaiTZg.sz0alhLsnWklU4L5SeNwr9UnwGY
-")
+bot.login('NjM0NDIzOTQzNDkwNDM3MTQw.XaiTZg.sz0alhLsnWklU4L5SeNwr9UnwGY')
